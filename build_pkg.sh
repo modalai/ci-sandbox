0194da50b8ec965c892dbd67974adbfc999e88d2
DemirKaya7 <74899222+DemirKaya7@users.noreply.github.com>
2023-06-12 12:49:31 -0700

    Update build_pkg.sh

--- build_pkg.sh
+++ build_pkg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 rm -rf voxl-px4
 git clone https://gitlab.com/voxl-public/voxl-sdk/services/voxl-px4.git
 
@@ -7,7 +7,6 @@ cd voxl-px4
 ls
 git submodule update --init --recursive
 
-#cd voxl-px4
 #./clean.sh
 #./build.sh
 
